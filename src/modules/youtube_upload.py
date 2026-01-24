7e5bc2f4e18be9b369b4dfe5a3e54be86b019c70
shimizu <ayoaho@yahoo.co.jp>
2026-01-24 18:39:39 +0900

    エラーを修正

--- src/modules/youtube_upload.py
+++ src/modules/youtube_upload.py
@@ -2,6 +2,7 @@
 
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from pathlib import Path
